8770cedc077800c298cb715a8f44cd0d17a215d9
Dev <Dev@10.0.2.15>
2016-12-20 09:33:57 +0100

    Recalcul du panier fonctionnel

---
+++ Controlleur/calculPrixPanier.php
@@ -0,0 +1,24 @@
+<?php  
+        require('../Model/Produit.php');
+        require('../Model/LigneCommande.php');
+        require('../Model/Panier.php');
+        session_start();
+        require('../DAO/toutboisDAO.php');
+        
+        if(isset($_SESSION['id'])) {
+
+            for ($i=0;$i<count($_SESSION['panier']->getLigneDeCommande());$i++){
+                
+                $quantite=$_POST[$_SESSION['panier']->getLigneDeCommande()[$i]->getProduit()->getCodeProduit()];
+                //echo $quantite;
+                $_SESSION['panier']->getLigneDeCommande()[$i]->setQuantite($quantite);
+            }
+            
+            header("location:../Controlleur/panier.php");
+      
+        }
+        else
+        {
+            header("location:../Controlleur/index.php");
+        }
+?>
---
+++ Controlleur/validationCommande.php
@@ -0,0 +1,12 @@
+<?php 
+require('../Vue/Head.php');
+require('../Vue/Menu.php');
+require('../Vue/Login.php');
+require('../Vue/Footer.php');
+
+afficheEntete(1);
+afficheMenu();
+afficheLogin();
+afficheFooter(1);
+?>
+
